d36d7167463e2a90de22eecbe93941b547e7644b
Behzadkk <bhzdkkvnd@gmail.com>
2019-07-15 13:27:58 +0100

    React build folder to repo

---
+++ precache-manifest.12970b5be5cd795d8af53f4c51f19d3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14867a265e0c93543bfe45625626d80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef981862a4e1e9db018e",
+    "url": "/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ef981862a4e1e9db018e",
+    "url": "/static/js/main.7e6d32ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
